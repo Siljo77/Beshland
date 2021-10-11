e30488934c4879e106c3dc272ea89a78a9eda3b2
Siljo77 <ivan.sijan@gmail.com>
2021-10-11 18:36:35 +0200

    commit to bes2

---
+++ bes.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+app.run(debug=True, port=5005)
