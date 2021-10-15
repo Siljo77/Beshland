ccdee20bcf58656878c893867d9428166d86b281
Ivan Sijan <ivan.sijan@gmail.com>
2021-10-15 14:24:17 +0200

    commit on master

--- bes.py
+++ bes.py
@@ -1,9 +1,27 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-import os
+from  flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_TRACK-MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+# Initialize the database
+db = SQLAlchemy(app)
+
+#Create db model
+class Friends(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    data_created = db.Columne(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):  
+        return '<Name %r>' % self.id
+
+
+subscribes = []
 
 @app.route('/')
 @app.route('/home')
@@ -20,9 +38,11 @@ def about():
 
 @app.route('/gallery')
 def gallery():
-    images = ["case.jpeg", "klinci/jpeg", "loncarstvo.jpg"]
+    images_row = ["zdjela_gline.jpeg", "loncarstvo.jpg", "klinci.jpg"]
+    images_row_1 = ["vaza.jpeg", "radione.jpeg", "radionica_klinci.jpeg"]
+    images_row_2 = ["vaze.jpeg", "velika_zdjela.jpeg", "case.jpeg"]
     name = 'Photo Gallery'
-    return render_template('gallery.html', name=name, images=images)
+    return render_template('gallery.html', name=name, images_row=images_row, images_row_1=images_row_1, images_row_2=images_row_2)
 
 
 @app.route('/store')
@@ -35,15 +55,6 @@ def shop():
 def workshops():
     name = 'Workshops'
     return render_template('workshops.html', name=name)
-
-
-@app.route('/proba_galerija')
-def proba_galerija():
-    images_row = ["zdjela_gline.jpeg", "loncarstvo.jpg", "klinci.jpg"]
-    images_row_1 = ["vaza.jpeg", "radione.jpeg", "radionica_klinci.jpeg"]
-    images_row_2 = ["vaze.jpeg", "velika_zdjela.jpeg", "case.jpeg"]
-    name = 'Proba Galerija'
-    return render_template('proba_galerija.html', name=name, images_row=images_row, images_row_1=images_row_1, images_row_2=images_row_2)
 
 
 app.run(debug=True)
