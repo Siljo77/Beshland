089596236f0d4c548180684f8204cf9d952b6268
Ivan Sijan <ivan.sijan@gmail.com>
2021-10-22 10:07:49 +0200

    Nemogu jos uc u log in

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, request
+from flask import Flask, render_template, flash, request, redirect, url_for
 from flask.wrappers import Request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
@@ -6,7 +6,7 @@ from wtforms.validators import DataRequired, EqualTo, Length
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 
 
 app = Flask(__name__)
@@ -16,10 +16,52 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ivansijan:medvescak77@loca
 # Initialize the database
 db = SQLAlchemy(app)
 
+# Flask_Login staff
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return users.query.get(int(user_id))
 
 
-class users(db.Model):
+#Create Login Form
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+    
+    
+#Create Loign page
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = users.query.filter_by(username=form.username.data).first()
+        if user:
+            # Check the hash
+            if check_password_hash(user.password_hash, form.password.data):
+                load_user(user)
+                flash('Login Successfull')
+                return redirect(url_for('dashboard'))
+            else:
+                flash('Wrong Password -- Try Again!')
+        else:
+            flash("That User Dosn't Exist! Try")
+    return render_template("login.html", form=form)
+
+
+#Create Dashboard page
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    return render_template("dashboard.html")
+
+
+class users(db.Model, UserMixin):
     id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
     name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(100),nullable=False, unique=True )
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
@@ -41,6 +83,7 @@ class users(db.Model):
         
 class UserForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
+    username =  StringField("Username", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired()])
     password_hash = PasswordField("Password", validators=[DataRequired(), EqualTo('password_hash2', message= 'Passwords Must Match!')]) 
     password_hash2 = PasswordField("Confirm Password", validators=[DataRequired()])                              
@@ -64,7 +107,15 @@ def update(id):
             return render_template("update.html", form=form, name_to_update=name_to_update)
     else:
         return render_template("update.html", form=form, name_to_update=name_to_update)
-           
+
+#Create Logout Page
+@app.route("/logout", methods=['GET', 'POST'])
+@login_required
+def logout():
+    load_user()
+    flash('You Have Been Logged Out!')
+    return redirect(url_for('login'))
+
 
 class NamerForm(FlaskForm):
     name = StringField("What's Your Name", validators=[DataRequired()])                   
@@ -79,14 +130,15 @@ def add_user():
         user = users.query.filter_by(email=form.email.data).first()
         if user is None:
             hashed_pw = generate_password_hash(form.password_hash.data, "sha256")
-            user = users(name=form.name.data, email=form.email.data, password_hash=hashed_pw)
+            user = users( name=form.name.data, username=form.username.data, email=form.email.data, password_hash=hashed_pw)
             db.session.add(user)
             db.session.commit()
             flash("User Submmitted Successfully")
         elif user is not None:
-            flash("User already has an account")
+            flash("Already has an account")
         name = form.name.data
         form.name.data = ''
+        form.username.data = ''
         form.email.data = ''
         form.password_hash = ''
     
