911f655b8538005a15e307d8a77200730ce47868
Ivan Sijan <ivan.sijan@gmail.com>
2021-11-05 09:51:37 +0100

    Nova forma i popravak na amidn_routes

--- admin/forms.py
+++ admin/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms.validators import DataRequired, EqualTo, Email
+from wtforms.validators import DataRequired, EqualTo, Email, Length
 from wtforms import StringField, SubmitField, PasswordField
 
 
 class UserForm(FlaskForm):
-    name = StringField("Full Name", validators=[DataRequired()])
+    name = StringField("Full Name", validators=[DataRequired(Length(min=2, max=100))])
     username = StringField("Username", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired(), Email()])
     password_hash = PasswordField("Password", validators=[DataRequired(), EqualTo('password_hash2', message= 'Passwords Must Match!')]) 
@@ -18,3 +18,7 @@ class LoginForm(FlaskForm):
     submit = SubmitField("Login")
 
 
+class UpdateForm(FlaskForm):
+    name = StringField("Full Name", validators=[DataRequired()])
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
