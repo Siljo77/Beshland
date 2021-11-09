e22692404b9ed4c7ec46189d419654449b54b465
Siljo77 <ivan.sijan@gmail.com>
2021-11-09 21:34:26 +0100

    update requirements

--- db.py
+++ db.py
@@ -2,8 +2,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-
-
 def initDb(app):
     app.config['SECRET_KEY'] = 'medvescak77'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ivansijan:medvescak77@localhost/users'
