4ef7ff232f3f490cccfa4889065e4ece5ef15a0c
Siljo77 <ivan.sijan@gmail.com>
2021-12-09 15:01:31 +0100

    galerija u procecu

--- admin/routes.py
+++ admin/routes.py
@@ -40,11 +40,10 @@ def about():
 @login_required
 def gallery():
     page_name = 'Gallery'
-    images_row = ["bes_slika1.jpeg", "bes_slika2.jpeg", "bes_slika3.jpeg", "bes_slika4.jpeg",
-                  "bes_slika5.jpeg", "radionica_klinci.jpeg", "vaze.jpeg", "velika_zdjela.jpeg", "case.jpeg","bes_slika1.jpeg"]
-    for item_name in images_row:
-        item_name
-    return render_template('admin/gallery.html', page_name=page_name, images_row=images_row, item_name=item_name)
+    images_row = {"bes_slika1.jpeg":'Zastori', "bes_slika2.jpeg":'Lampe', "bes_slika3.jpeg":'Casa', "bes_slika4.jpeg":'Radovi',
+                 "bes_slika5.jpeg":'Stol', "radionica_klinci.jpeg":"Djeca", "vaze.jpeg":'Vaze', "velika_zdjela.jpeg":'Kolo', "case.jpeg":'Sarene case'}
+    
+    return render_template('admin/gallery.html', page_name=page_name, images_row=images_row)
 
 
 #Create Dashboard page
