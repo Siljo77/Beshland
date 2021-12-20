08b651a0a0d894b5f542ccd5d4ff06178b630c37
Siljo77 <ivan.sijan@gmail.com>
2021-12-20 15:21:57 +0100

    mac verzija

--- app.py
+++ app.py
@@ -1,27 +1,23 @@
 import os
 from flask import Flask
-from db import initDb, db
-
+from public.routes import public_routes
+from admin.routes import admin_routes
+from admin.login_manager import login_manager, startLogin
+from admin.users import Users
+from db import initDb,db
+from public.routes import index as public_index
 
 template_dir = os.path.abspath('./templates')
 app = Flask(__name__, template_folder=template_dir)
 
-
-initDb(app)
-
-from admin.products import Products, Products2
-with app.app_context():
-    db.create_all()
-
-
-from public.routes import public_routes, index as public_index
-from admin.routes import admin_routes
-from admin.login_manager import login_manager, startLogin
-from admin.users import Users
-
 app.register_blueprint(public_routes, url_prefix="/public")
 app.register_blueprint(admin_routes, url_prefix="/admin")
 startLogin(app)
+initDb(app)
+
+with app.app_context():
+    db.create_all()
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -32,4 +28,4 @@ def index():
     return public_index()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=5001)
